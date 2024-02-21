e519d05f15009328fd47d250ced58b9c24a043f6
ssoheilmhd <57831603+ssoheilmhd@users.noreply.github.com>
2024-02-22 01:17:41 +0330

    Update baleAPIaccess.py

--- baleAPIaccess.py
+++ baleAPIaccess.py
@@ -1,13 +1,17 @@
 import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+baleAPItoken = os.environ['baleAPItoken']
+url = f'https://tapi.bale.ai/bot{baleAPItoken}'
 
 def GetUpdateFromUsers(Method):
     """
     This is created to find new user's message
     """
-    url = 'https://tapi.bale.ai/bot1064197082:x1cIcTfyNrXxBSFfPUUaxG2Ybv5BOe9bLIgXcvbI/%s' % Method
     #-1 in offset is last message which is sent by users
     payload = {'offset': -1 }
-    response = requests.get(url,params=payload)
+    response = requests.get(url+f'/{Method}',params=payload)
     chat_id = response.json()['result'][0]['message']['from']['id']
     message_id = response.json()['result'][0]['message']['message_id']
     text = response.json()['result'][0]['message']['text']
@@ -17,7 +21,6 @@ def AnswerToUser(Method,chat_id,text,message_id):
     """
     This function is created to send the chatgpt's response to the user
     """
-    url = 'https://tapi.bale.ai/bot1064197082:x1cIcTfyNrXxBSFfPUUaxG2Ybv5BOe9bLIgXcvbI/%s' % Method
     payload = { 'chat_id':chat_id, 'text':text, 'reply_to_message_id':message_id }
-    response = requests.post(url,params=payload)
+    response = requests.post(url+f'/{Method}',params=payload)
     print (response)
