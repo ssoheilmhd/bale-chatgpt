ea5648e95ad6080117c6012845fa2bb80a8ae915
ssoheilmhd <57831603+ssoheilmhd@users.noreply.github.com>
2024-02-22 01:46:55 +0330

    Update chatgptCaller.py

--- chatgptCaller.py
+++ chatgptCaller.py
@@ -1,8 +1,11 @@
-OPENAI_API_KEY="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"
 from openai import OpenAI
+import os
+from dotenv import load_dotenv
+load_dotenv()
+baleAPItoken = os.environ['baleAPItoken']
 
 def SendQuestionToChatgpt(content):
-    client = OpenAI(api_key=OPENAI_API_KEY)
+    client = OpenAI(api_key=baleAPItoken)
     completion = client.chat.completions.create(
       model="gpt-3.5-turbo",
       messages=[
