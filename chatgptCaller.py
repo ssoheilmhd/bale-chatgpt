db954d460543c1cf2e2764bba957aa7f02ccb625
ssoheilmhd <57831603+ssoheilmhd@users.noreply.github.com>
2024-02-22 01:50:31 +0330

    Update chatgptCaller.py

--- chatgptCaller.py
+++ chatgptCaller.py
@@ -5,7 +5,11 @@ load_dotenv()
 baleAPItoken = os.environ['baleAPItoken']
 
 def SendQuestionToChatgpt(content):
+    """
+    This function asks chatgpt your question and gets the answer.
+    """
     client = OpenAI(api_key=baleAPItoken)
+    # You can change the chatgpt version here in model
     completion = client.chat.completions.create(
       model="gpt-3.5-turbo",
       messages=[
