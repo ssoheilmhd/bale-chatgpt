67e090f4232c3ec43f505d14fb3201b58fb95ab7
ssoheilmhd <57831603+ssoheilmhd@users.noreply.github.com>
2024-02-22 02:18:07 +0330

    Update controller.py

--- controller.py
+++ controller.py
@@ -11,4 +11,7 @@ while True:
     check_message_id = message_id
     ChatgptResponse = SendQuestionToChatgpt(text)
     AnswerToUser("sendMessage",chat_id,ChatgptResponse,message_id)
+    #Chatgpt API free version has a certain limit on the number of requests, you can only send 20 requests per minute
+    #I decided to reply the last user after 3 seconds. It helps me to have maximum requests (20) in a minute
+    #It's cristal clear that if you buy a good license you can change this line.
     time.sleep(3)
