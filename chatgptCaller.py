802d4fc01573faf5296736c56e40a9383f8ffe7a
ssoheilmhd <57831603+ssoheilmhd@users.noreply.github.com>
2025-03-15 09:06:04 +0330

    Fix API key for chatgpt

--- chatgptCaller.py
+++ chatgptCaller.py
@@ -2,13 +2,13 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv
 load_dotenv()
-baleAPItoken = os.environ['baleAPItoken']
+openaiAPItoken = os.environ['openaiAPItoken']
 
 def SendQuestionToChatgpt(content):
     """
     This function asks chatgpt your question and gets the answer.
     """
-    client = OpenAI(api_key=baleAPItoken)
+    client = OpenAI(api_key=openaiAPItoken)
     # You can change the chatgpt version here in model
     completion = client.chat.completions.create(
       model="gpt-3.5-turbo",
